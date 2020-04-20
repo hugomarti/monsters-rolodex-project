4850d489da5cebf21eb69b99b96e61ab34b1b589
Hugo <apple@Apples-MBP.lan>
2020-04-20 10:47:21 +0800

    Added card list, card and search box

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+
 import './App.css';
 
 class App extends Component {
@@ -7,32 +10,31 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein',
-          id: 'as1'
-        },
-        {
-          name: 'Drakula',
-          id: 'de2'
-        },
-        {
-          name: 'Zombie',
-          id: 'dr3'
+      monsters: [],
+      searchField: ''
+    };
+  }
 
-        }
-      ]
-    }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(user => this.setState({ monsters: user }));
   }
 
   render() {
+
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className="App">
-        {
-          this.state.monsters.map(monster =>
-            <h1> key={monster.id}  {monster.name}</h1>
-          )
-        }
+        <SearchBox 
+        placeholder='search monsters'
+        handleChange={e => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     )
   }
